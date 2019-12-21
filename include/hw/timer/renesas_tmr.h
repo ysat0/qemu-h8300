68b277306bbd578909aae8ac8f2f10a82f1359e6
Yoshinori Sato <ysato@users.sourceforge.jp>
2019-12-21 22:46:57 +0900

    cleanup

--- include/hw/timer/renesas_tmr.h
+++ include/hw/timer/renesas_tmr.h
@@ -15,11 +15,6 @@
 #define TYPE_RENESAS_TMR "renesas-tmr"
 #define RTMR(obj) OBJECT_CHECK(RTMRState, (obj), TYPE_RENESAS_TMR)
 
-enum timer_event {cmia = 0,
-                  cmib = 1,
-                  ovi = 2,
-                  none = 3,
-                  TMR_NR_EVENTS = 4};
 enum {
     TMR_CH = 2,
     TMR_NR_IRQ = 3 * TMR_CH,
@@ -28,7 +23,7 @@ enum {
 typedef struct RTMRState {
     SysBusDevice parent_obj;
 
-    uint64_t input_freq;
+    int64_t input_freq;
     MemoryRegion memory;
 
     uint8_t tcnt[TMR_CH];
@@ -38,13 +33,11 @@ typedef struct RTMRState {
     uint8_t tccr[TMR_CH];
     uint8_t tcor[TMR_CH];
     uint8_t tcsr[TMR_CH];
-    int64_t tick;
-    int64_t div_round[TMR_CH];
-    enum timer_event next[TMR_CH];
+    int64_t last;
     qemu_irq cmia[TMR_CH];
     qemu_irq cmib[TMR_CH];
     qemu_irq ovi[TMR_CH];
-    QEMUTimer *timer[TMR_CH];
+    QEMUTimer *timer;
     uint32_t type;
 } RTMRState;
 
