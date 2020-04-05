a6cb1e6be2760091aa8f4edb1c66fa4b42447554
Yoshinori Sato <ysato@users.sourceforge.jp>
2020-04-05 22:55:46 +0900

    NE2000 local bus

---
+++ hw/net/ne2000-local.c
@@ -0,0 +1,162 @@
+/*
+ * QEMU NE2000 emulation -- CPU local bus windup
+ *
+ * Copyright (c) 2003-2004 Fabrice Bellard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "qemu/osdep.h"
+#include "hw/sysbus.h"
+#include "hw/net/ne2000-local.h"
+#include "hw/qdev.h"
+#include "ne2000.h"
+#include "sysemu/sysemu.h"
+#include "qapi/error.h"
+#include "qapi/visitor.h"
+
+#define TYPE_LOCAL_NE2000 "ne2k_local"
+
+#define LOCAL_NE2000(obj) OBJECT_CHECK(LOCALNE2000State, (obj), TYPE_LOCAL_NE2000)
+
+typedef struct LOCALNE2000State {
+    SysBusDevice parent_obj;
+
+    NE2000State ne2000;
+} LOCALNE2000State;
+
+static NetClientInfo net_ne2000_local_info = {
+    .type = NET_CLIENT_DRIVER_NIC,
+    .size = sizeof(NICState),
+    .receive = ne2000_receive,
+};
+
+static const VMStateDescription vmstate_local_ne2000 = {
+    .name = "ne2000",
+    .version_id = 2,
+    .minimum_version_id = 0,
+    .fields = (VMStateField[]) {
+        VMSTATE_STRUCT(ne2000, LOCALNE2000State, 0, vmstate_ne2000, NE2000State),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
+static void local_ne2000_realizefn(DeviceState *dev, Error **errp)
+{
+    SysBusDevice *qdev = SYS_BUS_DEVICE(dev);
+    LOCALNE2000State *nedev = LOCAL_NE2000(qdev);
+    NE2000State *ne = &nedev->ne2000;
+
+    ne2000_setup_io(ne, DEVICE(qdev), 0x20);
+    sysbus_init_mmio(qdev, &ne->io);
+    sysbus_init_irq(qdev, &ne->irq);
+
+    qemu_macaddr_default_if_unset(&ne->c.macaddr);
+    ne2000_reset(ne);
+
+    ne->nic = qemu_new_nic(&net_ne2000_local_info, &ne->c,
+                          object_get_typename(OBJECT(dev)), dev->id, ne);
+    qemu_format_nic_info_str(qemu_get_queue(ne->nic), ne->c.macaddr.a);
+}
+
+static Property ne2000_local_properties[] = {
+    DEFINE_NIC_PROPERTIES(LOCALNE2000State, ne2000.c),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+static void local_ne2000_class_initfn(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+
+    dc->realize = local_ne2000_realizefn;
+    dc->props = ne2000_local_properties;
+    dc->vmsd = &vmstate_local_ne2000;
+    set_bit(DEVICE_CATEGORY_NETWORK, dc->categories);
+}
+
+static void local_ne2000_get_bootindex(Object *obj, Visitor *v,
+                                       const char *name, void *opaque,
+                                       Error **errp)
+{
+    LOCALNE2000State *dev = LOCAL_NE2000(obj);
+    NE2000State *s = &dev->ne2000;
+
+    visit_type_int32(v, name, &s->c.bootindex, errp);
+}
+
+static void local_ne2000_set_bootindex(Object *obj, Visitor *v,
+                                       const char *name, void *opaque,
+                                       Error **errp)
+{
+    LOCALNE2000State *dev = LOCAL_NE2000(obj);
+    NE2000State *s = &dev->ne2000;
+    int32_t boot_index;
+    Error *local_err = NULL;
+
+    visit_type_int32(v, name, &boot_index, &local_err);
+    if (local_err) {
+        goto out;
+    }
+    /* check whether bootindex is present in fw_boot_order list  */
+    check_boot_index(boot_index, &local_err);
+    if (local_err) {
+        goto out;
+    }
+    /* change bootindex to a new one */
+    s->c.bootindex = boot_index;
+
+out:
+    error_propagate(errp, local_err);
+}
+
+static void local_ne2000_instance_init(Object *obj)
+{
+    object_property_add(obj, "bootindex", "int32",
+                        local_ne2000_get_bootindex,
+                        local_ne2000_set_bootindex, NULL, NULL, NULL);
+    object_property_set_int(obj, -1, "bootindex", NULL);
+}
+static const TypeInfo local_ne2000_info = {
+    .name          = TYPE_LOCAL_NE2000,
+    .parent        = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof(LOCALNE2000State),
+    .class_init    = local_ne2000_class_initfn,
+    .instance_init = local_ne2000_instance_init,
+};
+
+static void local_ne2000_register_types(void)
+{
+    type_register_static(&local_ne2000_info);
+}
+
+type_init(local_ne2000_register_types)
+
+void ne2000_init(NICInfo *nd, uint32_t base, qemu_irq irq)
+{
+    DeviceState *dev;
+    SysBusDevice *s;
+
+    qemu_check_nic_model(nd, "ne2000");
+    dev = qdev_create(NULL, TYPE_LOCAL_NE2000);
+    qdev_set_nic_properties(dev, nd);
+    qdev_init_nofail(dev);
+    s = SYS_BUS_DEVICE(dev);
+    sysbus_mmio_map(s, 0, base);
+    sysbus_connect_irq(s, 0, irq);
+}
+
