stsquad/qemu

hw/timer: RX62N internal timer modules
renesas_tmr: 8bit timer modules.
renesas_cmt: 16bit compare match timer modules.
This part use many renesas's CPU.
Hardware manual.
https://www.renesas.com/us/en/doc/products/mpumcu/doc/rx_family/r01uh0033ej0140_rx62n.pdf

Signed-off-by: Yoshinori Sato <ysato@users.sourceforge.jp>

include/hw/timer/renesas_tmr.h
@@ -1,0 +1,46 @@
+/*
+ * Renesas 8bit timer Object
+ *
+ * Copyright (c) 2018 Yoshinori Sato
+ *
+ * This code is licensed under the GPL version 2 or later.
+ *
+ */
+
+#ifndef HW_RENESAS_TMR_H
+#define HW_RENESAS_TMR_H
+
+#include "hw/sysbus.h"
+
+#define TYPE_RENESAS_TMR "renesas-tmr"
+#define RTMR(obj) OBJECT_CHECK(RTMRState, (obj), TYPE_RENESAS_TMR)
+
+enum timer_event {cmia = 0,
+                  cmib = 1,
+                  ovi = 2,
+                  none = 3,
+                  TMR_NR_EVENTS = 4};
+enum {CH = 2};
+typedef struct RTMRState {
+    SysBusDevice parent_obj;
+
+    uint64_t input_freq;
+    MemoryRegion memory;
+
+    uint8_t tcnt[CH];
+    uint8_t tcora[CH];
+    uint8_t tcorb[CH];
+    uint8_t tcr[CH];
+    uint8_t tccr[CH];
+    uint8_t tcor[CH];
+    uint8_t tcsr[CH];
+    int64_t tick;
+    int64_t div_round[CH];
+    enum timer_event next[CH];
+    qemu_irq cmia[CH];
+    qemu_irq cmib[CH];
+    qemu_irq ovi[CH];
+    QEMUTimer *timer[CH];
+} RTMRState;
+
+#endif

ysat0/qemu-h8300

include/hw/timer/renesas_tmr.h
@@ -3,43 +3,34 @@
  *
  * Copyright (c) 2018 Yoshinori Sato
  *
- * SPDX-License-Identifier: GPL-2.0-or-later
+ * This code is licensed under the GPL version 2 or later.
+ *
  */
 
-#ifndef HW_TIMER_RENESAS_TMR_H
-#define HW_TIMER_RENESAS_TMR_H
+#ifndef HW_RENESAS_TMR_H
+#define HW_RENESAS_TMR_H
 
-#include "qemu/timer.h"
 #include "hw/sysbus.h"
-#include "qom/object.h"
 
 #define TYPE_RENESAS_TMR "renesas-tmr"
-typedef struct RTMRState RTMRState;
-DECLARE_INSTANCE_CHECKER(RTMRState, RTMR,
-                         TYPE_RENESAS_TMR)
+#define RTMR(obj) OBJECT_CHECK(RTMRState, (obj), TYPE_RENESAS_TMR)
 
-enum timer_event {
-    cmia = 0,
-    cmib = 1,
-    ovi = 2,
-    none = 3,
-    TMR_NR_EVENTS = 4
-};
-
+enum timer_event {cmia = 0,
+                  cmib = 1,
+                  ovi = 2,
+                  none = 3,
+                  TMR_NR_EVENTS = 4};
 enum {
     TMR_CH = 2,
-    TMR_NR_IRQ = 3 * TMR_CH
+    TMR_NR_IRQ = 3 * TMR_CH,
 };
 
-struct RTMRState {
-    /*< private >*/
+typedef struct RTMRState {
     SysBusDevice parent_obj;
-    /*< public >*/
 
     uint64_t input_freq;
     MemoryRegion memory;
 
-    int64_t tick;
     uint8_t tcnt[TMR_CH];
     uint8_t tcora[TMR_CH];
     uint8_t tcorb[TMR_CH];
@@ -47,12 +38,13 @@ struct RTMRState {
     uint8_t tccr[TMR_CH];
     uint8_t tcor[TMR_CH];
     uint8_t tcsr[TMR_CH];
+    int64_t tick;
     int64_t div_round[TMR_CH];
-    uint8_t next[TMR_CH];
+    enum timer_event next[TMR_CH];
     qemu_irq cmia[TMR_CH];
     qemu_irq cmib[TMR_CH];
     qemu_irq ovi[TMR_CH];
-    QEMUTimer timer[TMR_CH];
-};
+    QEMUTimer *timer[TMR_CH];
+} RTMRState;
 
 #endif