b3978ac9ae85690d198a36d82b31e94db2ebbf8e
Yoshinori Sato <ysato@users.sourceforge.jp>
2022-07-06 17:13:34 +0900

    rx_icu.h v7.0.0

--- include/hw/intc/rx_icu.h
+++ include/hw/intc/rx_icu.h
@@ -1,14 +1,34 @@
-#ifndef RX_ICU_H
-#define RX_ICU_H
+/*
+ * RX Interrupt Control Unit
+ *
+ * Copyright (c) 2019 Yoshinori Sato
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include "qemu-common.h"
-#include "hw/irq.h"
+#ifndef HW_INTC_RX_ICU_H
+#define HW_INTC_RX_ICU_H
+
+#include "hw/sysbus.h"
+#include "qom/object.h"
 
 enum TRG_MODE {
     TRG_LEVEL = 0,
-    TRG_NEDGE = 1,	/* Falling */
-    TRG_PEDGE = 2,	/* Raising */
-    TRG_BEDGE = 3,	/* Both */
+    TRG_NEDGE = 1,      /* Falling */
+    TRG_PEDGE = 2,      /* Raising */
+    TRG_BEDGE = 3,      /* Both */
 };
 
 struct IRQSource {
@@ -17,20 +37,22 @@ struct IRQSource {
 };
 
 enum {
-    NR_IRQS = 256,
-    SWI = 27,     /* Software interrupt request */
+    /* Software interrupt request */
+    SWI = 27,
+    NR_IRQS = 256
 };
 
 struct RXICUState {
+    /*< private >*/
     SysBusDevice parent_obj;
+    /*< public >*/
 
     MemoryRegion memory;
     struct IRQSource src[NR_IRQS];
-    char *icutype;
     uint32_t nr_irqs;
-    uint32_t *map;
+    uint8_t *map;
     uint32_t nr_sense;
-    uint32_t *init_sense;
+    uint8_t *init_sense;
 
     uint8_t ir[NR_IRQS];
     uint8_t dtcer[NR_IRQS];
@@ -42,14 +64,13 @@ struct RXICUState {
     uint8_t nmier;
     uint8_t nmiclr;
     uint8_t nmicr;
-    int req_irq;
+    int16_t req_irq;
     qemu_irq _irq;
     qemu_irq _fir;
     qemu_irq _swi;
 };
-typedef struct RXICUState RXICUState;
 
-#define TYPE_RXICU "rx-icu"
-#define RXICU(obj) OBJECT_CHECK(RXICUState, (obj), TYPE_RXICU)
+#define TYPE_RX_ICU "rx-icu"
+OBJECT_DECLARE_SIMPLE_TYPE(RXICUState, RX_ICU)
 
 #endif /* RX_ICU_H */
