3ba318cf2d22eda7c231ccd86803d41621ac5b37
Yoshinori Sato <ysato@users.sourceforge.jp>
2022-07-06 16:38:49 +0900

    renesas_tmr.h v7.0.0

--- include/hw/timer/renesas_tmr.h
+++ include/hw/timer/renesas_tmr.h
@@ -3,29 +3,43 @@
  *
  * Copyright (c) 2018 Yoshinori Sato
  *
- * This code is licensed under the GPL version 2 or later.
- *
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-#ifndef HW_RENESAS_TMR_H
-#define HW_RENESAS_TMR_H
+#ifndef HW_TIMER_RENESAS_TMR_H
+#define HW_TIMER_RENESAS_TMR_H
 
+#include "qemu/timer.h"
 #include "hw/sysbus.h"
+#include "qom/object.h"
 
 #define TYPE_RENESAS_TMR "renesas-tmr"
-#define RTMR(obj) OBJECT_CHECK(RTMRState, (obj), TYPE_RENESAS_TMR)
+typedef struct RTMRState RTMRState;
+DECLARE_INSTANCE_CHECKER(RTMRState, RTMR,
+                         TYPE_RENESAS_TMR)
+
+enum timer_event {
+    cmia = 0,
+    cmib = 1,
+    ovi = 2,
+    none = 3,
+    TMR_NR_EVENTS = 4
+};
 
 enum {
     TMR_CH = 2,
-    TMR_NR_IRQ = 3 * TMR_CH,
+    TMR_NR_IRQ = 3 * TMR_CH
 };
 
-typedef struct RTMRState {
+struct RTMRState {
+    /*< private >*/
     SysBusDevice parent_obj;
+    /*< public >*/
 
-    int64_t input_freq;
+    uint64_t input_freq;
     MemoryRegion memory;
 
+    int64_t tick;
     uint8_t tcnt[TMR_CH];
     uint8_t tcora[TMR_CH];
     uint8_t tcorb[TMR_CH];
@@ -33,12 +47,12 @@ typedef struct RTMRState {
     uint8_t tccr[TMR_CH];
     uint8_t tcor[TMR_CH];
     uint8_t tcsr[TMR_CH];
-    int64_t last;
+    int64_t div_round[TMR_CH];
+    uint8_t next[TMR_CH];
     qemu_irq cmia[TMR_CH];
     qemu_irq cmib[TMR_CH];
     qemu_irq ovi[TMR_CH];
-    QEMUTimer *timer;
-    uint32_t type;
-} RTMRState;
+    QEMUTimer timer[TMR_CH];
+};
 
 #endif
